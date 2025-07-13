c903fab2d492e68486df85085378a2143efe2930
Honeyverma107 <153446175+Honeyverma107@users.noreply.github.com>
2025-07-13 14:03:01 +0530

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,12 @@
+
+
+class addition{
+    public static void main(String[] args) {
+        int x=8;
+        int y=9;
+        //int sum;
+        //sum =x+y;
+        System.out.println(x+y);
+
+    }
+}
